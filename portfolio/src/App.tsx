24994e05a0607764b780cce4166ce27060d6ae51
Daniel Perez <danielperezf98@gmail.com>
2023-12-28 12:20:25 -0800

    Updated file name

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { LandingPage } from "./pages/landingpage";
+import { LandingPage } from "./pages/LandingPage";
 import { SkillsPage } from "./pages/SkillsPage";
 
 function App() {
