39574e2de70f5d5bd9ae1eb04944b75e1dc7ed6e
Daniel Perez <danielperezf98@gmail.com>
2023-12-31 01:45:53 -0800

    import file name fix

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { LandingPage } from "./pages/LandingPage";
+import { LandingPage } from "./pages/landingpage";
 import { SkillsPage } from "./pages/SkillsPage";
 import { ProjectsPage } from "./pages/ProjectsPage";
 import { ProjectDetailsPage } from "./pages/ProjectDetailsPage";
