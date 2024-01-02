f6f3cad019ae72f9f1388e103362e8bd34bdc789
Daniel Perez <danielperezf98@gmail.com>
2024-01-02 15:36:08 -0800

    fixed scrolling issue

--- portfolio/src/pages/ProjectDetailsPage.tsx
+++ portfolio/src/pages/ProjectDetailsPage.tsx
@@ -1,8 +1,13 @@
+import { useEffect } from "react";
 import { DetailsFooter } from "../components/DetailsFooter";
 import { DetailsHeader } from "../components/DetailsHeader";
 import { ProjectDetails } from "../components/ProjectDetails";
 
 export function ProjectDetailsPage() {
+  useEffect(() => {
+    // Scroll to the top when the component mounts
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="h-screen bg-[#364652]">
       <DetailsHeader />
